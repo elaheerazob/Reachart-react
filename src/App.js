4ebd7ce8702d9ccded4611d7447eba38effa4123
elaheerazob <ridwanelahee282@gmail.com>
2022-03-28 12:42:22 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Reachart from './Reachart/reachart';
+
+function App() {
+  return (
+    <div className="App">
+      <Reachart></Reachart>
+    </div>
+  );
+}
+
+export default App;
